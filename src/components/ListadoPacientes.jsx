1b6f5ce1444c21c6841c93ed198e1d428ccf22d5
iAngelManuel <iangelmanuel@hotmail.com>
2023-06-05 15:09:40 -0500

    Cambio de estilos a ListadoPaciente

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -7,7 +7,7 @@ export default function ListadoPacientes({ pacientes, setPaciente, eliminarPacie
     <div className="md:w-1/2 lg:w-3/5 mx-5 md:h-screen md:overflow-y-scroll">
       {pacientes.length ? (
         <>
-          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
           <p className="text-lg mt-5 mb-5 text-center">Administra tus <span className="text-indigo-600 font-bold">Pacientes y Citas</span></p>
           {pacientes.map(paciente => (
             <Paciente
@@ -21,7 +21,7 @@ export default function ListadoPacientes({ pacientes, setPaciente, eliminarPacie
       ) : (
         <>
           <h2 className="font-black text-3xl text-center">No hay pacientes aún</h2>
-          <p className="text-xl mt-5 mb-5 text-center">Comienza agregando pacientes y <span className="text-indigo-600 font-bold">Apareceran en este lugar</span></p>
+          <p className="text-lg mt-5 mb-5 text-center">Comienza agregando pacientes y <span className="text-indigo-600 font-bold">Apareceran en este lugar</span></p>
         </>
       )}    
     </div>
